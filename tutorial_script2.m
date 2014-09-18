dd9ae40b3802d6e23b8e76c28523918daafc19f7
Thanuja Ambegoda <thanuja@ini.ch>
2014-09-18 14:39:29 +0200

    gg

--- tutorial_script2.m
+++ tutorial_script2.m
@@ -1,20 +1,25 @@
 %% 1. Create data sets by selecting either 1, 2 or 3 
+% Please select 1, 2 or 3 to pick a data set
 
 loadData = 1; 
 % loadData = 1 for linearly separable 2 classes
 % loadData = 2 for XOR data set
 % loadData = 3 for gaussian data points
 
+%% 2. don't have to change these lines in block 2
 numPoints = 100;  % number of data points for each class
+Y=[-ones(1,numPoints),ones(1,numPoints)];
+
 
 if(loadData==1)
 % set 1
 % linearly separable data set at y = 1
 
-dat1=[rand(1,numPoints);rand(1,numPoints)];   % class '+1'
-dat2=[rand(1,numPoints);1+rand(1,numPoints)]; % class '-1'
-X=[dat1,dat2];
+dat1=[rand(1,numPoints/2);rand(1,numPoints/2)];   % class '+1'
+dat2=[rand(1,numPoints/2);1+rand(1,numPoints/2)]; % class '-1'
+X =[dat1,dat2];
 data = X';
+size(data)
 end
 
 
@@ -23,17 +28,21 @@ if(loadData==2)
 dat1 = [randn(numPoints,2)/20 ; 1+randn(numPoints,2)/20];
 dat2 = [repmat([1,0],numPoints,1)+randn(numPoints,2)/20 ; repmat([0,1],numPoints,1)+randn(numPoints,2)/20];
 data = [dat1;dat2];
+size(data)
 end
 
 if(loadData==3)
 %creates gaussian datapoints with distance "dist" between the means
+
+dist = 5;
+
 dat1 = randn(numPoints,2);
 dat2 = sqrt(dist)+randn(numPoints,2);
 data = [dat1; dat2];
+size(data)
 end
 
-
-%% 2. start a for loop for learning the weights iteratively
+%% 3. TASK: start a for loop for learning the weights iteratively.
 
 % inside the loop, choose a random data point. randi(n) is the function that
 % can select a random integer between 1 and n.
